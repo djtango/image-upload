40ed939271487f0b24ba78ba63cd07503b7f07cf
deonjtan@gmail.com <deonjtan@gmail.com>
2016-02-15 20:31:47 +0000

    add validateFileFormat

--- js/upload.js
+++ js/upload.js
@@ -1,1 +1,5 @@
-
+function validateFileFormat(filePath) {
+  var fileExtension = filePath.split(".").pop().toLowerCase();
+  var allowedExtensions = {"jpg": true, "png": true, "gif": true};
+  return allowedExtensions[fileExtension];
+}
--- test/upload.spec.js
+++ test/upload.spec.js
@@ -1,5 +1,26 @@
-describe('first test', function() {
-  it('return true', function() {
-    expect(true).toBe(true);
+describe('validateFileFormat function:', function() {
+  it('returns true for jpg files', function() {
+    var testJpg = 'someJpg.jpg';
+    expect(validateFileFormat(testJpg)).toBe(true);
+  });
+
+  it('returns true for png files', function() {
+    var testPng = 'randomPng.png';
+    expect(validateFileFormat(testPng)).toBe(true);
+  });
+
+  it('returns true for gif files', function() {
+    var testGif = 'funnyGif.gif';
+    expect(validateFileFormat(testGif)).toBe(true);
+  });
+
+  it('returns false for a non-supported file format', function() {
+    var nonSupportedImage = 'sadBitmap.bmp';
+    expect(validateFileFormat(nonSupportedImage)).toBeFalsy();
+  });
+
+  it('returns false for a file with no file extension', function() {
+    var noFileExtension = 'fileWithNoExtension';
+    expect(validateFileFormat(noFileExtension)).toBeFalsy();
   });
 });
