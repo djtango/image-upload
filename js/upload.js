1e3485f4bdf326ae05eb025bfb5bb090eea1b5fb
deonjtan@gmail.com <deonjtan@gmail.com>
2016-02-15 20:44:36 +0000

    implement validation check to file upload field

--- js/upload.js
+++ js/upload.js
@@ -1,5 +1,16 @@
-function validateFileFormat(filePath) {
+function isValidFileFormat(filePath) {
   var fileExtension = filePath.split(".").pop().toLowerCase();
   var allowedExtensions = {"jpg": true, "png": true, "gif": true};
   return allowedExtensions[fileExtension];
 }
+
+window.onload = function() {
+  var fileInput = document.getElementById('file_upload');
+  fileInput.addEventListener("change", function(e) {
+    var notAllowedFileFormat = !isValidFileFormat(fileInput.value);
+    if(notAllowedFileFormat) {
+      window.alert("Error: unsupported image file format. Please submit the file as a jpg, png or gif.")
+      fileInput.value = "";
+    }
+  });
+};
--- test/upload.spec.js
+++ test/upload.spec.js
@@ -1,26 +1,26 @@
-describe('validateFileFormat function:', function() {
+describe('isValidFileFormat function:', function() {
   it('returns true for jpg files', function() {
     var testJpg = 'someJpg.jpg';
-    expect(validateFileFormat(testJpg)).toBe(true);
+    expect(isValidFileFormat(testJpg)).toBe(true);
   });
 
   it('returns true for png files', function() {
     var testPng = 'randomPng.png';
-    expect(validateFileFormat(testPng)).toBe(true);
+    expect(isValidFileFormat(testPng)).toBe(true);
   });
 
   it('returns true for gif files', function() {
     var testGif = 'funnyGif.gif';
-    expect(validateFileFormat(testGif)).toBe(true);
+    expect(isValidFileFormat(testGif)).toBe(true);
   });
 
   it('returns false for a non-supported file format', function() {
     var nonSupportedImage = 'sadBitmap.bmp';
-    expect(validateFileFormat(nonSupportedImage)).toBeFalsy();
+    expect(isValidFileFormat(nonSupportedImage)).toBeFalsy();
   });
 
   it('returns false for a file with no file extension', function() {
     var noFileExtension = 'fileWithNoExtension';
-    expect(validateFileFormat(noFileExtension)).toBeFalsy();
+    expect(isValidFileFormat(noFileExtension)).toBeFalsy();
   });
 });
